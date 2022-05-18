d05646f0c93abcf19262a552754cc7cb1e6aabe9
LAPTOP-A372VS6L\Kuya <aulya.khatulistivani@aqi.co.id>
2022-05-18 20:56:33 +0700

    change dashboard route to redirect siswa

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
   Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect()->route('siswa.index');
   })->name('dashboard');
 
   Route::get('/', function () {
